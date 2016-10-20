9acaefc5c021febe9e3a3acf333e09662838533d
Kuro <kuro_q@live.com>
2016-10-21 00:01:33 +0800

    create helloworld

---
+++ SourceCode/Helloworld/Helloworld.Basic/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Helloworld.Basic
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helloworld");
+            Console.ReadKey();
+        }
+    }
+}
