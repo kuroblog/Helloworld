b60d3a71dddf06808cef4cd77f1f75b64c2c40b0
Kuro <kuro_q@live.com>
2016-12-01 21:31:13 +0800

    create mef demo 01

---
+++ SourceCode/Helloworld/K.MEF.Basic.01/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace K.MEF.Basic
+{
+    using System.ComponentModel.Composition;
+    using System.ComponentModel.Composition.Hosting;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new ApplicationRoot();
+            app.Run();
+        }
+    }
+
+    class ApplicationRoot
+    {
+        private void Compose()
+        {
+            var compositionContainer = new CompositionContainer();
+            compositionContainer.ComposeParts(this);
+        }
+
+        public void Run()
+        {
+            Compose();
+        }
+    }
+}
---
+++ SourceCode/Helloworld/K.MEF.Basic/Program.cs
@@ -0,0 +1,69 @@
+﻿
+namespace K.MEF.Basic
+{
+    using System;
+    using System.ComponentModel.Composition;
+    using System.ComponentModel.Composition.Hosting;
+    using System.ComponentModel.Composition.Registration;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FluentExportInterfaceDemo();
+        }
+
+        private static void FluentExportInterfaceDemo()
+        {
+            Program program = new Program();
+            var picker = new RegistrationBuilder();
+            picker.ForTypesMatching(pl => pl.Name.EndsWith("Plugin"))
+               .ExportInterfaces();
+
+            var catalog = new AssemblyCatalog(typeof(Program).Assembly, picker);
+
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(program);
+
+            var log = container.GetExportedValue<ILogPlugin>();
+            var save = container.GetExportedValue<ISavePlugin>();
+
+            save.Save("test");
+            log.Write("test");
+        }
+    }
+
+    public class GreatEditor
+    {
+        public ILogPlugin Logger { get; set; }
+        public ISavePlugin Saver { get; set; }
+    }
+
+
+    public interface ILogPlugin
+    {
+        void Write(string message);
+    }
+
+    public class LoggerPlugin : ILogPlugin
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine("Logger {0}", message);
+        }
+    }
+
+    public interface ISavePlugin
+    {
+        void Save(string message);
+    }
+
+    public class FilePlugin : ISavePlugin
+    {
+        public void Save(string message)
+        {
+            Console.WriteLine("FilePlugin {0}", message);
+
+        }
+    }
+}
